c169237a594360524f788562eb48bf9f74f3bf7a
jhunsley <john.hunsley@capgemini.com>
2020-02-25 17:06:08 +0000

    readings level service stereotype

--- src/main/java/com/hunsley/ea/client/StationLevelService.java
+++ src/main/java/com/hunsley/ea/client/StationLevelService.java
@@ -3,10 +3,21 @@ package com.hunsley.ea.client;
 import com.hunsley.ea.client.model.response.ReadingsResponse;
 import java.time.LocalDate;
 
+/**
+ * Finds the Level Readings for a given station id.
+ *
+ * @author jhunsley
+ */
 public interface StationLevelService {
 
+  /**
+   * https://environment.data.gov.uk/flood-monitoring/id/stations/{id}/readings?latest
+   */
   ReadingsResponse getLatestLevelReadings(int stationId);
 
+  /**
+   * https://environment.data.gov.uk/flood-monitoring/id/stations/{id}/readings?today&_sorted
+   */
   ReadingsResponse getTodaysLevelReadings(int stationId);
 
   ReadingsResponse getLevelReadingsFromDate(int stationId, LocalDate from);
