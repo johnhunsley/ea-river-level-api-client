2fe8fa00ea3f632067faf10382bd1aaeb3a146b6
jhunsley <john.hunsley@capgemini.com>
2020-02-26 15:11:02 +0000

    doc

--- src/main/java/com/hunsley/ea/client/ReadingsService.java
+++ src/main/java/com/hunsley/ea/client/ReadingsService.java
@@ -2,7 +2,6 @@ package com.hunsley.ea.client;
 
 import com.hunsley.ea.client.model.EaApiClientException;
 import com.hunsley.ea.client.model.response.ReadingsResponse;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -28,7 +27,7 @@ public interface ReadingsService {
   ReadingsResponse getTodaysLevelReadings(int stationId) throws EaApiClientException;
 
   /**
-   *  eturn the readings taken since the given date time (not inclusive), up to the specified _limit.
+   *  Return the readings taken since the given date time (not inclusive), up to the specified _limit.
    *  If no _limit is given then a default limit of 500 will be used. Typically when tracking a particular measurement
    *  then use the dateTime of the last retrieved value as the since parameter to find any new readings.
    *  Will accept a simple date value such as 2016-09-07 which will be interpreted as 2016-09-07T:00:00:00Z.
